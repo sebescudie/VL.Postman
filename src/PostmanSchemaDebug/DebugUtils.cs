a9edff87d8641c53e797f52a0e6cdee033616e7b
sebescudie <accounts@sebescudie.fr>
2022-01-16 20:35:03 +0100

    WIP recursion

--- src/PostmanSchemaDebug/DebugUtils.cs
+++ src/PostmanSchemaDebug/DebugUtils.cs
@@ -2,17 +2,64 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 using System.Threading.Tasks;
 
 namespace PostmanSchemaDebug
 {
     class DebugUtils
     {
+        internal const int RootLevel = 0;
+
         // Retunrs true of the Postman item is a folder
         public static bool IsFolderItem(PostmanSchemaDebug.Items item)
         {
             return item.Request == null;
+        }
 
+        public static void PrintCollectionTree(Items item)
+        {
+            try
+            {
+                var rootDirectory = item;
+                PrintCollectionTree(rootDirectory, RootLevel);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error : " + e.Message);
+            }
+        }
+
+        private static void PrintCollectionTree(Items item, int currentLevel)
+        {
+            var indentation = string.Empty;
+
+            for (var i = RootLevel; i < currentLevel; i++)
+            {
+                indentation += "-";
+            }
+
+            Console.WriteLine($"{indentation}-{item.Name}");
+            
+            var nextLevel = currentLevel + 1;
+            try
+            {
+                if(item.Item != null)
+                {
+                    foreach (var subDirectory in item.Item)
+                    {
+                        PrintCollectionTree(subDirectory, nextLevel);
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error : " + e.Message);
+            }
         }
     }
 }
--- src/PostmanSchemaDebug/Program.cs
+++ src/PostmanSchemaDebug/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace PostmanSchemaDebug
 {
@@ -8,13 +10,43 @@ namespace PostmanSchemaDebug
         static void Main(string[] args)
         {
             var coordinate = Coordinate.FromJson(System.IO.File.ReadAllText(@"D:\documents\dev\vvvv\libraries\VL.Postman\help\postman\ACME.postman_collection.json"));
+
             foreach(var item in coordinate.Item)
             {
-                Console.WriteLine("===");
-                Console.WriteLine(item.Name);
-                Console.WriteLine("Is it a folder?");
-                Console.WriteLine(DebugUtils.IsFolderItem(item));
-                coordinate
+                if(DebugUtils.IsFolderItem(item))
+                {
+                    // If we are in a folder, we iterate over all its nodes and instantiate them
+                    CreateNodesInFolder(item, item.Name);
+                }
+                else
+                {
+                    // Otherwise, it means we're at the root level so just create the node
+                    CreateNode(item, coordinate.Info.Name);
+                }
+            }
+
+            void CreateNodesInFolder(Items folder, string category)
+            {
+                // We're looking at all the elements in the folder
+                for(int i = 0; i < folder.Item.Count; i++)
+                {
+                    // If we hit a folder...
+                    if(DebugUtils.IsFolderItem(folder.Item[i]))
+                    {
+                        // We recurse!
+                        CreateNodesInFolder(folder.Item[i], folder.Name + "." + folder.Item[i].Name);
+                    }
+                    // Otherwise we're just looking at a query so just query it
+                    else
+                    {
+                        CreateNode(folder.Item[i], category);
+                    }
+                }
+            }
+
+            void CreateNode(Items item, string category)
+            {
+                Console.WriteLine(item.Name + "(" + category + ")");
             }
         }
     }
